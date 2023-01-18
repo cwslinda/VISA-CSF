76dd7f8f0f32010b6a1735ad2c5b03e3a7cfc6b3
cwslinda <lindacheung94@gmail.com>
2023-01-18 14:35:26 +0800

    day 33 material

---
+++ day33style/src/app/app.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'day33style';
+
+  form!: FormGroup
+
+  constructor(private fb: FormBuilder){
+    
+  }
+
+  processForm(){
+    console.info("form >>", this.form.value)
+  }
+
+  clearForm(){
+    this.form = this.createForm()
+  }
+
+  ngOnInit(): void {
+    this.form = this.createForm()
+  }
+
+
+  private createForm(): FormGroup{
+    return this.fb.group({
+      name: this.fb.control<string>("", [Validators.required]),
+      email: this.fb.control<string>("", [Validators.required, Validators.email]),
+      dob: this.fb.control<Date>(new Date(), [Validators.required]),
+    })
+  }
+}
